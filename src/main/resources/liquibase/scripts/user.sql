fb7ff2860473dd3fde9bdd6f2748644266ce0268
ivanh <ivanhlop@gmail.com>
2024-04-30 15:42:21 +0300

    add liquibase

---
+++ src/main/resources/liquibase/scripts/user.sql
@@ -0,0 +1,68 @@
+-- liquibase formatted sql
+
+-- changeset ihlopachev:1
+
+CREATE TABLE ad
+(
+    id     UUID PRIMARY KEY,
+    author TEXT,
+    image  TEXT,
+    pk     BIGINT,
+    price  DOUBLE PRECISION,
+    title  TEXT
+)
+;
+CREATE TABLE comment
+(
+    id              UUID PRIMARY KEY,
+    author          TEXT,
+    authorImage     TEXT,
+    authorFirstName TEXT,
+    createdAt       TIMESTAMP,
+    pk              BIGINT,
+    text            TEXT
+);
+CREATE TABLE create_or_update_ad
+(
+    id          UUID PRIMARY KEY,
+    title       TEXT,
+    price       DOUBLE PRECISION,
+    description TEXT
+);
+CREATE TABLE create_or_update_comment
+(
+    id   UUID PRIMARY KEY,
+    text TEXT
+);
+CREATE TABLE login
+(
+    id       UUID PRIMARY KEY,
+    username TEXT,
+    password TEXT
+);
+CREATE TABLE new_password
+(
+    id              UUID PRIMARY KEY,
+    currentPassword TEXT,
+    newPassword     TEXT
+);
+CREATE TABLE register
+(
+    id         UUID PRIMARY KEY,
+    username   TEXT,
+    password   TEXT,
+    first_name TEXT,
+    last_name  TEXT,
+    phone      TEXT,
+    role       TEXT
+);
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    email      TEXT,
+    first_name TEXT,
+    last_name  TEXT,
+    phone      TEXT,
+    role       TEXT,
+    image      TEXT
+);
