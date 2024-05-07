6838a745ad854c6f3c74a10788406c6451d2a614
ivanh <ivanhlop@gmail.com>
2024-05-07 14:58:56 +0300

    fix

--- src/main/java/selfConstructed/SkySalerADS/mapper/AdMapper.java
+++ src/main/java/selfConstructed/SkySalerADS/mapper/AdMapper.java
@@ -1,12 +1,17 @@
 package selfConstructed.SkySalerADS.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import selfConstructed.SkySalerADS.dto.AdDTO;
 import selfConstructed.SkySalerADS.model.Ad;
 
 @Mapper(componentModel = "spring")
 public interface AdMapper {
+    @Mapping(target = "pk", source = "pk")
+    @Mapping(target = "author", source = "author.id")
     AdDTO toDto(Ad ad);
 
+    @Mapping(target = "pk", source = "pk")
+    @Mapping(target = "author.id", source = "author")
     Ad toModel(AdDTO adDTO);
 }
