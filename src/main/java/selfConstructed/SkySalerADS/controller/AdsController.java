39d4d788b1b6d3bda3035a1eee3d0e83b87e1e82
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-05-13 09:04:33 +0300

    ads  @GetMapping

--- src/main/java/selfConstructed/SkySalerADS/controller/AdsController.java
+++ src/main/java/selfConstructed/SkySalerADS/controller/AdsController.java
@@ -1,48 +1,44 @@
-//package selfConstructed.SkySalerADS.controller;
+package selfConstructed.SkySalerADS.controller;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import selfConstructed.SkySalerADS.dto.AdsAllDTO;
+import selfConstructed.SkySalerADS.service.AdService;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequestMapping("/ads")
+@RequiredArgsConstructor
+public class AdsController {
 //
-//import io.swagger.v3.oas.annotations.media.Content;
-//import io.swagger.v3.oas.annotations.media.Schema;
-//import io.swagger.v3.oas.annotations.responses.ApiResponse;
-//import io.swagger.v3.oas.annotations.responses.ApiResponses;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.multipart.MultipartFile;
-//import selfConstructed.SkySalerADS.dto.AdDTO;
-//import selfConstructed.SkySalerADS.dto.AdsAllDTO;
-//import selfConstructed.SkySalerADS.dto.PreAdDTO;
-//import selfConstructed.SkySalerADS.service.AdService;
-//
-//@Slf4j
-//@CrossOrigin(value = "http://localhost:3000")
-//@RestController
-//@RequestMapping("/ads")
-//@RequiredArgsConstructor
-//public class AdsController {
-//
-//    private final AdService adService;
-//
-//    /**
-//     * get All ads
-//     */
-//    @ApiResponses({
-//            @ApiResponse(
-//                    responseCode = "200",
-//                    description = "Got all ads",
-//                    content = @Content(
-//                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-//                            schema = @Schema(implementation = AdsAllDTO.class)
-//                    )
-//            )
-//    })
-//    @GetMapping
-//    public ResponseEntity<AdsAllDTO> getAllAds() {
-//        return new ResponseEntity<>(adService.getAllAdsDTO(), HttpStatus.OK);
-//    }
+    private final AdService adService;
+
+    /**
+     * get All ads
+     */
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Got all ads",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = AdsAllDTO.class)
+                    )
+            )
+    })
+    @GetMapping
+    public ResponseEntity<AdsAllDTO> getAllAds() {
+        return new ResponseEntity<>(adService.getAllAdsDTO(), HttpStatus.OK);
+    }
 //
 //    /**
 //     * Create new ads
@@ -77,4 +73,4 @@
 //                                             @RequestParam(value = "image") MultipartFile[] files) {
 //        return new ResponseEntity<>(adService.createAd(preAdDTO, files), HttpStatus.CREATED);
 //    }
-//}
+}
