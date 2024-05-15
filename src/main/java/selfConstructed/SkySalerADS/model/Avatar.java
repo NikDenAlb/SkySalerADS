467d801045845bf1913714d7d1e9b8b62d613de0
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-05-15 21:26:55 +0300

    Avatar for now

--- src/main/java/selfConstructed/SkySalerADS/model/Avatar.java
+++ src/main/java/selfConstructed/SkySalerADS/model/Avatar.java
@@ -4,10 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.Hibernate;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Getter
